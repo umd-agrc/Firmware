8b8451fbe1b8e6603f3252d372d0682949ff8cf0
James Goppert <james.goppert@gmail.com>
2016-09-30 10:50:49 -0400

    Add support for vision speed message and integrate with LPE.

--- src/modules/local_position_estimator/BlockLocalPositionEstimator.cpp
+++ src/modules/local_position_estimator/BlockLocalPositionEstimator.cpp
@@ -35,7 +35,8 @@ BlockLocalPositionEstimator::BlockLocalPositionEstimator() :
 	// gps 10 hz
 	_sub_gps(ORB_ID(vehicle_gps_position), 1000 / 10, 0, &getSubscriptions()),
 	// vision 30 hz
-	_sub_vision_pos(ORB_ID(vision_position_estimate), 1000 / 30, 0, &getSubscriptions()),
+	_sub_vision(ORB_ID(vision_position_estimate), 1000 / 30, 0, &getSubscriptions()),
+	_sub_vision_speed(ORB_ID(vision_speed_estimate), 1000 / 30, 0, &getSubscriptions()),
 	// mocap 50 hz
 	_sub_mocap(ORB_ID(att_pos_mocap), 1000 / 50, 0, &getSubscriptions()),
 	// all distance sensors, 10 hz
@@ -77,6 +78,8 @@ BlockLocalPositionEstimator::BlockLocalPositionEstimator() :
 	_gps_epv_max(this, "EPV_MAX"),
 	_vision_xy_stddev(this, "VIS_XY"),
 	_vision_z_stddev(this, "VIS_Z"),
+	_vision_vxy_stddev(this, "VIS_VXY"),
+	_vision_vz_stddev(this, "VIS_VZ"),
 	_vision_on(this, "VIS_ON"),
 	_mocap_p_stddev(this, "VIC_P"),
 	_flow_gyro_comp(this, "FLW_GYRO_CMP"),
@@ -107,6 +110,7 @@ BlockLocalPositionEstimator::BlockLocalPositionEstimator() :
 	_lidarStats(this, ""),
 	_flowQStats(this, ""),
 	_visionStats(this, ""),
+	_visionVelocityStats(this, ""),
 	_mocapStats(this, ""),
 	_gpsStats(this, ""),
 
@@ -133,6 +137,7 @@ BlockLocalPositionEstimator::BlockLocalPositionEstimator() :
 	_time_last_sonar(0),
 	_time_init_sonar(0),
 	_time_last_vision_p(0),
+	_time_last_vision_vel(0),
 	_time_last_mocap(0),
 
 	// initialization flags
@@ -143,6 +148,7 @@ BlockLocalPositionEstimator::BlockLocalPositionEstimator() :
 	_sonarInitialized(false),
 	_flowInitialized(false),
 	_visionInitialized(false),
+	_visionVelocityInitialized(false),
 	_mocapInitialized(false),
 
 	// reference altitudes
@@ -167,6 +173,7 @@ BlockLocalPositionEstimator::BlockLocalPositionEstimator() :
 	_flowFault(FAULT_NONE),
 	_sonarFault(FAULT_NONE),
 	_visionFault(FAULT_NONE),
+	_visionVelocityFault(FAULT_NONE),
 	_mocapFault(FAULT_NONE),
 
 	// loop performance
@@ -283,7 +290,8 @@ void BlockLocalPositionEstimator::update()
 	bool paramsUpdated = _sub_param_update.updated();
 	bool baroUpdated = _sub_sensor.updated();
 	bool gpsUpdated = _gps_on.get() && _sub_gps.updated();
-	bool visionUpdated = _vision_on.get() && _sub_vision_pos.updated();
+	bool visionUpdated = _sub_vision.updated();
+	bool visionVelocityUpdated = _sub_vision_speed.updated();
 	bool mocapUpdated = _sub_mocap.updated();
 	bool lidarUpdated = (_sub_lidar != NULL) && _sub_lidar->updated();
 	bool sonarUpdated = (_sub_sonar != NULL) && _sub_sonar->updated();
@@ -476,12 +484,21 @@ void BlockLocalPositionEstimator::update()
 		}
 	}
 
-	if (visionUpdated) {
+	if (_vision_on.get() && visionUpdated) {
 		if (!_visionInitialized) {
 			visionInit();
 
 		} else {
 			visionCorrect();
+		}
+	}
+
+	if (_vision_on.get() && visionVelocityUpdated) {
+		if (!_visionVelocityInitialized) {
+			visionVelocityInit();
+
+		} else {
+			visionVelocityCorrect();
 		}
 	}
 
@@ -559,6 +576,7 @@ void BlockLocalPositionEstimator::checkTimeouts()
 	gpsCheckTimeout();
 	flowCheckTimeout();
 	visionCheckTimeout();
+	visionVelocityCheckTimeout();
 	mocapCheckTimeout();
 }
 
@@ -734,6 +752,7 @@ void BlockLocalPositionEstimator::publishEstimatorStatus()
 		+ ((_flowFault > FAULT_NONE) << SENSOR_FLOW)
 		+ ((_sonarFault > FAULT_NONE) << SENSOR_SONAR)
 		+ ((_visionFault > FAULT_NONE) << SENSOR_VISION)
+		+ ((_visionVelocityFault > FAULT_NONE) << SENSOR_VISION_VELOCITY)
 		+ ((_mocapFault > FAULT_NONE) << SENSOR_MOCAP);
 	_pub_est_status.get().timeout_flags =
 		(_baroInitialized << SENSOR_BARO)
@@ -742,6 +761,7 @@ void BlockLocalPositionEstimator::publishEstimatorStatus()
 		+ (_lidarInitialized << SENSOR_LIDAR)
 		+ (_sonarInitialized << SENSOR_SONAR)
 		+ (_visionInitialized << SENSOR_VISION)
+		+ (_visionVelocityInitialized << SENSOR_VISION_VELOCITY)
 		+ (_mocapInitialized << SENSOR_MOCAP);
 	_pub_est_status.get().pos_horiz_accuracy = _pub_gpos.get().eph;
 	_pub_est_status.get().pos_vert_accuracy = _pub_gpos.get().epv;
